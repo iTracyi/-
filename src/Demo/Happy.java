2f84febd099adcaa0882ec853b7ceec5b9f2c14e
iTracyi <806619801@qq.com>
2018-01-02 16:18:13 +0800

    实训周练习

---
+++ src/Demo/Happy.java
@@ -0,0 +1,25 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc=new Scanner(System.in);
+     System.out.println("请输入一个数");
+     System.out.println("请输入一个数");
+     System.out.println("请输入一个数");
+     System.out.println("请输入一个数");
+     System.out.println("请输入一个数");
+	int c=sc.nextInt();
+	shuchu(c);
+	}
+
+    public static void shuchu(int a){
+    	for (int i = 0; i <=a; i++){
+	    System.out.println(a);
+		}	
+}
+}	
+    
